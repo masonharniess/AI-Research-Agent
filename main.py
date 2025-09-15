3f57c5d7470fddc3bcf4e85fa07510bcf3dfcb0e
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-09-15 20:07:05 +0100

    test: check OpenAI invocation
    This commit adds a small snippet of code to main.py which verifies that invocations to the OpenAI LLM are working.

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 
 load_dotenv() # load .env file to obtain credentials
+
+llmOpenAI = ChatOpenAI(model="gpt-4o-mini")
+llmAnthropic = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+response = llmOpenAI.invoke("What is the meaning of life?")
+print(response)
