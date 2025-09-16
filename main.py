32e6390d52f0b442ca53dbdf071ca5f2d4b2549f
Mason Harniess <102977508+masonharniess@users.noreply.github.com>
2025-09-16 20:14:42 +0100

    feat: add response model, parser, and prompt template
    This commit creates three things:
    - A simple Python class 'ResearchResponse', which acts as the schema/template for the LLM's output.
    - A parser, which converts the LLM's output into the `ResearchResponse` model, allowing it to be used as a standard Python object in the code.
    -  A prompt template which structures any queries given to the LLM so that it can be provided enough information on how to achieve the intended outcome.

--- main.py
+++ main.py
@@ -2,11 +2,40 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv() # load .env file to obtain credentials
 
-llmOpenAI = ChatOpenAI(model="gpt-4o-mini")
-llmAnthropic = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+class ResearchResponse(BaseModel):
+  topic: str
+  summary: str
+  sources: list[str]
+  tools_used: list[str]
 
-response = llmOpenAI.invoke("What is the meaning of life?")
-print(response)
+llmOpenAI = ChatOpenAI(model="gpt-4o-mini")
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+  [
+    (
+      "system",
+      """
+      You are a research assistant that will help generate a research paper. 
+      Answer the user query and use the necessary tools.
+      Wrap the output in this format and provide no other text.
+      \n{format_instructions}
+      """,
+    ),
+    ("placeholder", "{chat_history}"),
+    ("human", "{query}"),
+    ("placeholder", "{agent_scratchpad}"),
+  ]
+).partial(format_instructions=parser.get_format_instructions())
+
+
+# llmAnthropic = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+# response = llmOpenAI.invoke("What is the meaning of life?")
+# print(response)
